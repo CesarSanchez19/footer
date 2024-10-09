9ab4e8eef631109749ef95326fc8e4d9fcfb7bc6
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2024-10-09 08:39:09 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Obtener el elemento HTML con el id 'year' y actualizar su contenido de texto
+// con el año actual obtenido mediante el objeto Date de JavaScript.
+document.getElementById('year').textContent = new Date().getFullYear();
